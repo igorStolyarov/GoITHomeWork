37a99fcc182d2fc5041c937ed1050851952bb4e2
igorStolyarov <terema13@gmail.com>
2016-06-08 16:55:09 +0300

    first sort arrays

--- src/com/goit/gojavaonlinethree/student/igorstolyarov/module05/arraysMinToMax/RunMinToMax.java
+++ src/com/goit/gojavaonlinethree/student/igorstolyarov/module05/arraysMinToMax/RunMinToMax.java
@@ -1,6 +1,8 @@
 package com.goit.gojavaonlinethree.student.igorstolyarov.module05.arraysMinToMax;
 
-public class RunMinToMax {
+public class RunMinToMax extends SortValueOnArrayMinToMax {
 
+    public static void main(String[] args) {
 
+    }
 }
--- src/com/goit/gojavaonlinethree/student/igorstolyarov/module05/arraysMinToMax/SortValueOnArrayMinToMax.java
+++ src/com/goit/gojavaonlinethree/student/igorstolyarov/module05/arraysMinToMax/SortValueOnArrayMinToMax.java
@@ -6,20 +6,17 @@ public class SortValueOnArrayMinToMax {
 
     Random rand = new Random();
 
-    private int[] arrMinToMax = new int[13];
-
-
+    public int[] arrMinToMax = new int[13];
 
     public int getArrMinToMax() {
         int minimum = arrMinToMax[0];
         for (int i = 0; i <= arrMinToMax.length - 1; i++) {
             arrMinToMax[i] = rand.nextInt(20);
-            System.out.println(arrMinToMax[i]);
-
         }
         return minimum;
-   }
+    }
 }
+
 
 
 
