c3b73430dea991493f84a8d53790d52608ef481d
igorStolyarov <terema13@gmail.com>
2016-06-09 15:08:56 +0300

    work array

--- src/com/goit/gojavaonlinethree/student/igorstolyarov/module05/arraysSort/RunMinToMax.java
+++
@@ -1,8 +0,0 @@
-package com.goit.gojavaonlinethree.student.igorstolyarov.module05.arraysSort;
-
-public class RunMinToMax extends SortValueOnArrayMinToMax {
-
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/com/goit/gojavaonlinethree/student/igorstolyarov/module051/InterfaceArray.java
@@ -0,0 +1,14 @@
+package com.goit.gojavaonlinethree.student.igorstolyarov.module051;
+
+/**
+ * Created by Igor on 09.06.2016.
+ */
+
+    // интерфейс для массива
+
+public interface InterfaceArray {
+
+    void sort();
+    String toString();
+    int searchMin();
+}
