6e732e554af3425f1a6f47b5b7b28861875c5c70
igorStolyarov <terema13@gmail.com>
2016-06-21 11:04:40 +0300

    homeWorkModuleSeven

--- src/com/goit/gojavaonlinethree/student/igorstolyarov/module061/RunnerForConverterTemperaturs.java
+++ src/com/goit/gojavaonlinethree/student/igorstolyarov/module061/RunnerForConverterTemperaturs.java
@@ -2,11 +2,34 @@ package com.goit.gojavaonlinethree.student.igorstolyarov.module061;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
-import static com.goit.gojavaonlinethree.student.igorstolyarov.module061.Text.*;
+
+import static com.goit.gojavaonlinethree.student.igorstolyarov.module061.Text.hi;
+import static com.goit.gojavaonlinethree.student.igorstolyarov.module061.Text.rezultText;
 
 public class RunnerForConverterTemperaturs extends ConverterTemperaturs {
 
-      public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
+
+          /* ДЗ модуль 7
+          необходимо:
+          Найти и описать анти-паттерны в своём коде и предложить вариант их исправления.
+          Формат описания - комментарии в исходном коде с предложениями их решения.
+          Решение обсудить с ментором и только ПОСЛЕ этого изменять код.
+
+
+             1.В классе runner
+                1.1. Выносим scanner в отдельный класс
+                1.2. Для exception создаем отдельный класс
+                1.3. Зацикливаем пользователя до введения корректного значения
+
+             2. В классе text
+                2.1 Переименовать строковую переменную hi
+
+             3. Онаруженные у себя недостатки в коде
+                3.1 Не всегда названия методов отображают суть того, что они делают
+                3.2 Есть проблема с пониманием когда работать через статический импорт,
+                    а когда через геттеры/сеттеры
+           */
 
         Scanner scanner = new Scanner(System.in);
 
@@ -14,15 +37,16 @@ public class RunnerForConverterTemperaturs extends ConverterTemperaturs {
         System.out.println(hi);
         // начинаем отлавливать ошибку
 
-         try {
-              celsiyToPharinghate = scanner.nextDouble();
+        // делаем do wile
+        try {
+            celsiyToPharinghate = scanner.nextDouble();
         /*
         если введена не цифра, то вылазит InputMismatchException
          */
-          } catch (InputMismatchException e) {
-              System.out.println("[Error:] Введите цифру");
-              return;
-          }
+        } catch (InputMismatchException e) {
+            System.out.println("[Error:] Введите цифру");
+            return;
+        }
         System.out.println(rezultText + "\n" + getAndReturn());
     }
 }
